1e6b90726b9577078b422eb36047c76419ddd059
badcat301 <badcat301@gmail.com>
2017-05-05 13:19:45 +0300

    Commit

--- lessons7/src/EntryPoint.java
+++ lessons7/src/EntryPoint.java
@@ -6,10 +6,10 @@ import util.ConsoleHelper;
 public class EntryPoint {
     public static void main(String[] args)  {
         String c = ConsoleHelper.getLine("Enter you city: LA, Paris, Krasnodar");
-
+        try {
         CityFactory factory = new CityFactory();
          City city = factory.getCity(c);
-try {
+
     System.out.println(city.getCity());
 }catch (CityNotFoundException e){
     System.out.println(e.getMessage());
